73c91f9473b367a176d4b20f8aedf4d14d225cf2
Akauê Lima <sk2_skt@hotmail.com>
2020-12-21 08:04:03 -0300

    Update CartPage.java

--- testWebINM/src/main/java/pages/CartPage.java
+++ testWebINM/src/main/java/pages/CartPage.java
@@ -7,12 +7,13 @@ public class CartPage extends BasePage {
 
 	public CartPage(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
+	//insere dados solicitados antes de finalizar compra
 	public CartPage insert_information() {
 		
-		driver.findElement(By.id("first-name")).sendKeys("Akau�");	
+		driver.findElement(By.id("first-name")).sendKeys("Akauê");	
 		driver.findElement(By.id("last-name")).sendKeys("Lima");	
 		driver.findElement(By.id("postal-code")).sendKeys("06852470");
 		driver.findElement(By.xpath("//input[@class=\"btn_primary cart_button\"]")).click();
@@ -20,7 +21,7 @@ public class CartPage extends BasePage {
 		return this;		
 	}
 	
-	
+	//botão para finalizar compra
 	public CartPage btn_finish() {	
 		
 	    driver.findElement(By.xpath("//a[@class=\"btn_action cart_button\"]")).click();	
